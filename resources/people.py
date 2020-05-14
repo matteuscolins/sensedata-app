e09a6312f264db216f14b25bedfa52f048dbdf6d
Matteus Colins <matteus.colins@ceuma.br>
2020-05-13 21:26:29 -0300

    Pagination completed

--- resources/people.py
+++ resources/people.py
@@ -1,11 +1,21 @@
 import functools
 from models.people import People
+from flask_paginate import Pagination, get_page_args
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
 
 bp = Blueprint('people', __name__, url_prefix='/people')
 
+people = People.get_all()
+
+def get_people(offset=0, per_page=10):
+    return people[offset: offset + per_page]
+
 @bp.route('/', methods=('GET', 'POST'))
 def home():
-    data = People.get_all()
-    data.sort(key=lambda x: x.name, reverse=False)
-    return render_template('people/people.html', people = data)
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')   
+    people.sort(key=lambda x: x.name, reverse=False)
+    total=len(people)
+    pagination_people = get_people(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    return render_template('people/people.html', people = pagination_people, page = page, pagination=pagination, per_page=per_page)
