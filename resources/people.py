3ec8befc65fb0b7b112387a902f1e953bb6d3133
Matteus Colins <matteus.colins@ceuma.br>
2020-05-14 10:57:15 -0300

    Add filters

--- resources/people.py
+++ resources/people.py
@@ -13,9 +13,53 @@ def get_people(offset=0, per_page=10):
 @bp.route('/', methods=('GET', 'POST'))
 def home():
     page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')   
-    people.sort(key=lambda x: x.name, reverse=False)
+    total=len(people)
+    pagination_people = get_people(offset=offset, per_page=per_page)
+    pagination = Pagination(page = page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    return render_template('people/people.html', people = pagination_people, page = page, 
+                                                pagination=pagination, per_page=per_page)
+
+@bp.route('/orderByName', methods=('GET', 'POST'))
+def order_ny_name():
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')   
+    people.sort(key=lambda x: x.name)
     total=len(people)
     pagination_people = get_people(offset=offset, per_page=per_page)
     pagination = Pagination(page=page, per_page=per_page, total=total,
                             css_framework='bootstrap4')
-    return render_template('people/people.html', people = pagination_people, page = page, pagination=pagination, per_page=per_page)
+    return render_template('people/people.html', people = pagination_people, 
+                    page = page, pagination=pagination, per_page=per_page)
+
+@bp.route('/orderByGender', methods=('GET', 'POST'))
+def order_by_gender():
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')   
+    people.sort(key=lambda x: x.gender)
+    total=len(people)
+    pagination_people = get_people(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    return render_template('people/people.html', people = pagination_people, 
+                    page = page, pagination=pagination, per_page=per_page)
+
+@bp.route('/orderByMass', methods=('GET', 'POST'))
+def order_by_mass():
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')   
+    people.sort(key=lambda x: x.mass)
+    total=len(people)
+    pagination_people = get_people(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    return render_template('people/people.html', people = pagination_people, 
+                    page = page, pagination=pagination, per_page=per_page)
+
+@bp.route('/orderByHeight', methods=('GET', 'POST'))
+def order_by_height():
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')   
+    people.sort(key=lambda x: x.height)
+    total=len(people)
+    pagination_people = get_people(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    return render_template('people/people.html', people = pagination_people, 
+                    page = page, pagination=pagination, per_page=per_page)
