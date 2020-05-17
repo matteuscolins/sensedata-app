c7ea49eb80fb0e8aaa309f286ec36b412beb7acd
Matteus Colins <matteus.colins@ceuma.br>
2020-05-17 16:40:31 -0300

    Add CORS lib

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 from resources.people import bp as bp_people
 from resources.starship import bp as bp_starship
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
