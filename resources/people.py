646913f7fc33512df947637557cc1fb85468814d
Matteus Colins <matteus.colins@ceuma.br>
2020-05-17 13:32:11 -0300

    Add filters

--- resources/people.py
+++ resources/people.py
@@ -1,5 +1,8 @@
 import functools
 from models.people import People
+from models.film import Film
+from models.vehicle import Vehicle
+from models.starship import Starship
 from flask_paginate import Pagination, get_page_args
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
 
@@ -21,7 +24,7 @@ def home():
                                                 pagination=pagination, per_page=per_page)
 
 @bp.route('/orderByName', methods=('GET', 'POST'))
-def order_ny_name():
+def order_by_name():
     page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')   
     people.sort(key=lambda x: x.name)
     total=len(people)
@@ -63,3 +66,74 @@ def order_by_height():
                             css_framework='bootstrap4')
     return render_template('people/people.html', people = pagination_people, 
                     page = page, pagination=pagination, per_page=per_page)
+
+@bp.route('/filter', methods=('GET', 'POST'))
+def filter():
+    people_filtered = []
+    description = request.form['description']
+    type_filter = request.form['filter']
+    if type_filter == 'film':
+        people_filtered = filter_by_film(description)
+    if type_filter == 'vehicle':
+        people_filtered = filter_by_vehicle(description)
+    if type_filter == 'starship':
+        people_filtered = filter_by_starship(description)
+    
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page') 
+    total=len(people_filtered)
+    pagination_people = get_people(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    return render_template('people/people.html', people = people_filtered, 
+                    page = page, pagination=pagination, per_page=per_page)
+
+def filter_by_film(description):
+    films = Film.get_all()
+    films_filtered = []
+    people_filtered = []
+    for i in range(len(films)):
+        if description.lower() in films[i].title.lower():
+            films_filtered.append(films[i])
+    
+    if films_filtered is not None:
+        for i in range(len(films_filtered)):
+            for j in range(len(people)):
+                for k in range(len(people[j].films)):
+                    if people[j].films[k] == films_filtered[i].url:
+                        people_filtered.append(people[j])
+
+    return people_filtered
+
+def filter_by_vehicle(description):
+    vehicles = Vehicle.get_all()
+    vehicles_filtered = []
+    people_filtered = []
+    for i in range(len(vehicles)):
+        if description.lower() in vehicles[i].name.lower():
+            vehicles_filtered.append(vehicles[i])
+
+    if vehicles_filtered is not None:
+        for i in range(len(vehicles_filtered)):
+            for j in range(len(people)):
+                for k in range(len(people[j].vehicles)):
+                    if people[j].vehicles[k] == vehicles_filtered[i].url:
+                        people_filtered.append(people[j])
+
+    return people_filtered
+
+def filter_by_starship(description):
+    starships = Starship.get_all()
+    starships_filtered = []
+    people_filtered = []
+    for i in range(len(starships)):
+        if description.lower() in starships[i].name.lower():
+            starships_filtered.append(starships[i])
+
+    if starships_filtered is not None:
+        for i in range(len(starships_filtered)):
+            for j in range(len(people)):
+                for k in range(len(people[j].starships)):
+                    if people[j].starships[k] == starships_filtered[i].url:
+                        people_filtered.append(people[j])
+
+    return people_filtered
